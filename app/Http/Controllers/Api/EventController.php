f6d3e3a66302c0a8d103ed762adb3431ad0b2095
wstudionatali <wstudionatali@gmail.com>
2024-01-09 15:01:36 +0100

    creating endpoint for all events and single event

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+/*Added*/
+use \App\Models\Event;
 
 class EventController extends Controller
 {
@@ -12,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        return Event::all();
     }
 
     /**
@@ -26,9 +28,9 @@ class EventController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        return $event;
     }
 
     /**
