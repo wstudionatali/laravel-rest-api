e4dcc16a2e320ec1426dfb567d3227aea02ee2f3
wstudionatali <wstudionatali@gmail.com>
2024-02-28 21:13:54 +0100

    Throtling the API

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -19,6 +19,7 @@ class AttendeeController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')->only(['store', 'destroy']);
         $this->authorizeResource(Attendee::class, 'attendee');
     }
     /**
@@ -42,7 +43,7 @@ class AttendeeController extends Controller
         //
         $attendee = $this->loadRelationships(
             $event->attendees()->create([
-                'user_id' => 1
+                'user_id' => $request->user()->id
             ])
         );
 
